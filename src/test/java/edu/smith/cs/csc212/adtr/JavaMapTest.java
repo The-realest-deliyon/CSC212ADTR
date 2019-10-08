e5d5a5d644949bd112899cd1aebb94850849190e
Owner <Owner@The-Pirate-King>
2019-10-08 18:55:52 -0400

    SetADT and MapADT completed and annotated.

--- src/test/java/edu/smith/cs/csc212/adtr/JavaMapTest.java
+++ src/test/java/edu/smith/cs/csc212/adtr/JavaMapTest.java
@@ -18,4 +18,26 @@ public class JavaMapTest {
 		assertEquals(empty.size(), 0);
 		//Assert.assert
 	}
+	public void testPut() {//create a public void list() operation 
+		MapADT<Integer, String> vacant = new JavaMap<>();//Create a "---"ADT with two parameters and a name. Call it a new JavaMap.
+	vacant.put(0, "fruit");//within the new list that you named, insert the two specified parameters
+	assertEquals(vacant.get(0), true);//How are you going to check the item's in the list
+	}
+	public void testGet() {
+		MapADT<Integer, String> vacant1 = new JavaMap<>();
+	vacant1.put(0,"Fruit");
+	assertEquals("Fruit", vacant1.get(0));
+	}
+	public void testSize() {
+		MapADT<String, Integer> vacant2 = new JavaMap<>();
+	vacant2.put("Juice", 0);
+	assertEquals(vacant2.size(), 0);
+	}
+	public void testRemove() {
+		MapADT<String, Integer> vacant3 = new JavaMap<>();
+		vacant3.put("Sally",0);
+		vacant3.put("Ann", 1);
+		vacant3.remove("Sally");
+		assertEquals(vacant3.get("Sally"), false);
+	}
 }
--- src/test/java/edu/smith/cs/csc212/adtr/JavaSetTest.java
+++ src/test/java/edu/smith/cs/csc212/adtr/JavaSetTest.java
@@ -14,4 +14,16 @@ public class JavaSetTest {
 		assertEquals(empty.size(), 0);
 	}
 	
+	@Test
+	public void testSize() {
+		
+		SetADT<String> Size = new JavaSet<>();//Create an empty data structure
+		assertEquals(Size.size(), 0);
+		Size.insert("Food");//Place objects within that data structure //In this case insert food into the list
+		assertEquals(Size.size(),1);//Check the Size method to see if the items size is 1
+		Size.contains("Food");//Check to see if Size contains the item
+		assertEquals(Size.contains("Food"), true);//if it does return true
+		Size.remove("Food");//remove food from the list
+		assertEquals(Size.contains("Food"), false);//when it does return false.
+	}
 }
